8c289bc87fbcb163011a56f7f36f9e6f4ae1488d
Mikey71212 <michaelholton712@gmail.com>
2022-10-20 17:32:01 +1100

    improved handleClick

--- src/TodoControls.js
+++ src/TodoControls.js
@@ -1,16 +1,6 @@
-import Todo from "./Todo"
-
 const TodoControls = ({ sortTodos, orderLabel, setListMode }) => {
-	const handleClickPending = () => {
-		setListMode("pending")
-	}
-
-	const handleClickComplete = () => {
-		setListMode("complete")
-	}
-
-	const handleClickAll = () => {
-		setListMode("all")
+	const handleClick = (mode) => {
+		setListMode(mode)
 	}
 
 	return (
@@ -18,13 +8,13 @@ const TodoControls = ({ sortTodos, orderLabel, setListMode }) => {
 			<button className="btn btn-light" onClick={sortTodos}>
 				{orderLabel}
 			</button>
-			<button className="btn btn-light" onClick={handleClickPending}>
+			<button className="btn btn-light" onClick={() => handleClick("pending")}>
 				Show Pending
 			</button>
-			<button className="btn btn-light" onClick={handleClickComplete}>
+			<button className="btn btn-light" onClick={() => handleClick("complete")}>
 				Show Completed
 			</button>
-			<button className="btn btn-light" onClick={handleClickAll}>
+			<button className="btn btn-light" onClick={() => handleClick("all")}>
 				Show All
 			</button>
 		</div>
